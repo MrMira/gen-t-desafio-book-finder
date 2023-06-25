dac6acade681994fea49303fd8b328bd24455feb
Miguel Mira <miguel.mrmira@gmail.com>
2023-06-24 23:00:58 -0300

    Estilizado os componentes de listagem dos livros

--- src/components/CardBook.vue
+++ src/components/CardBook.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+import noImageUrl from '../assets/noimage.png';
+
 const props = defineProps([
   'titulo',
   'autores',
@@ -7,19 +9,49 @@ const props = defineProps([
   'imageUrl'
 ]);
 
+
 let autoresFormatted = props.autores.join(', ');
 let dataPublicacaoFormatted = props.dataPublicacao.tipo === 'ano' ? props.dataPublicacao.valor.format('YYYY') : props.dataPublicacao.valor.format('DD/MM/YYYY');
+let imageUrlFormatted = props.imageUrl == null ? noImageUrl : props.imageUrl;
 
 </script>
 
 <template>
-  <div>
-    <img :src="imageUrl" />
-    <p>{{ titulo }}</p>
-    <p>{{ autoresFormatted }}</p>
-    <p>{{ dataPublicacaoFormatted }}</p>
+  <div class="card-book">
+    <div class="card-image">
+      <img :src="imageUrlFormatted" />
+    </div>
+    <div class="card-content">
+      <p class="card-content__title">{{ titulo }}</p>
+      <p class="card-content__author">{{ autoresFormatted }}</p>
+      <p class="card-content__date">{{ dataPublicacaoFormatted }}</p>
+      <a href="#">Acessar livro no Google Books</a>
+    </div>
   </div>
 </template>
 
 <style scoped>
+  .card-book {
+    display: flex;
+    padding: 10px 5px;
+    margin: 30px 50px;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+  }
+
+  .card-content p {
+    margin: 10px 15px;
+  }
+
+  .card-content a {
+    margin: 10px 15px;
+    text-decoration: none;
+  }
+
+  .card-content__title {
+    font-weight: bold;
+  }
+
+  .card-content__author {
+    font-style: italic;
+  }
 </style>
--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -7,13 +7,20 @@
   import GoogleBooksApi from '../services/GoogleBooksAPI.js';
   
   let dataSource = ref([]);
+  let dataLoading = ref('');
+  let textNoDataAvailable = ref('');
 
   function handleSearchRequest(text) {
-      function success(dataRaw) {
+    
+    function success(dataRaw) {
+        textNoDataAvailable.value = '';
+        dataLoading.value = 'loader';
         dataSource.value = GoogleBooksApi.ParseBookItems(dataRaw);
+        dataLoading.value = '';
       }
 
       function error() {
+        textNoDataAvailable.value = 'Ocorreu um erro ao tentar fazer a busca pelos livros. Por favor, tente mais tarde...';
         dataSource.value = [];
       }
 
@@ -22,16 +29,57 @@
 </script>
 
 <template>
-  <div>
-    <h1>Book Finder</h1>
-  </div>
-  <div>
-    <Searcher @search-requested="handleSearchRequest" />
-  </div>
-  <div>
-    <ListCard :data-source="dataSource" />
+  <div class="page-book">
+    <div class="book-head">
+      <h1 class="book-head__title">Buscador de Livros</h1>
+      <Searcher @search-requested="handleSearchRequest" />
+    </div>
+    <div class="book-body">
+      <ListCard :data-source="dataSource" />
+      <div class="body-help">
+        <div :class="{dataLoading}"></div>
+        <p class="body-help__no-data">{{textNoDataAvailable}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+  .book-head {
+    margin: 20px 0;
+    text-align: center;
+  }
+
+  .book-head__title {
+    margin-bottom: 10px;
+  }
+
+  .body-help {
+    margin-top: 50px;
+    text-align: center;
+  }
+
+  .body-help__no-data {
+    font-style: italic;
+  }
+
+  .loader {
+    width: 48px;
+    height: 48px;
+    border: 5px solid #444;
+    border-bottom-color: transparent;
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+    }
+
+    @keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+  } 
 </style>
